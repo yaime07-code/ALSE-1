02dc9f7f5a6c5e8a051ed1e2c73c9ea13bee7cc4
yaime07 <jaime.enrique.galvis@gmail.com>
2025-08-30 09:21:43 -0500

    ot

---
+++ otro.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Include the standard input/output library for printf
+
+// Main function: entry point of the program
+int main(int argc, char** argv) {
+    // Print 'Hello World.' to the console
+    std::cout << "Hello World.\n";
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
