e0cd81f8ae1eca0fb476a0e94337a56a2a6566ce
yaime07 <jaime.enrique.galvis@gmail.com>
2025-08-30 08:46:51 -0500

    second

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 int main(int argc, char** argv) {
     // Print 'Hello World.' to the console
     std::cout << "Hello World.\n";
-
+    int a = 2;
+    std::cout << "The value of a is: " << a << "\n";
+    double b = 2.3;
+    std::cout << "The value of b is: " << b << "\n";
     // Return 0 to indicate successful execution
     return 0;
 }
