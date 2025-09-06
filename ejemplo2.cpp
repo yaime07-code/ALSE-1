fa2a08804506b87dad86b4c2d9201b68dcd4154f
yaime07 <jaime.enrique.galvis@gmail.com>
2025-09-06 09:32:22 -0500

    example2

---
+++ ejemplo2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+struct Point {
+    int x, y;
+};
+
+int main() {
+    Point p1, p2;
+    p1.x = 10;
+    p1.y = 20;
+    p2.x = 30;
+    p2.y = 40;
+    char character = 't';
+    int a = 89;
+    float b = 3.14;
+    cout<<"char + int:" <<character + a << endl;
+    cout<<"float + int:" <<b + a << endl;
+    cout<<"char + float:" <<character + b << endl;
+    p2.x++;
+    a =p2.y++;
+    int c=++a;
+    c+= 8;
+    c *= 3;  
+
+    return 0;
+
+
+}
