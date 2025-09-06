30c466cf10cd38ac9dd57584b076f087c4a791d9
yaime07 <jaime.enrique.galvis@gmail.com>
2025-09-06 09:03:02 -0500

    example2 def

--- example.cpp
+++ example.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
-#define debug
-long double largeDecimal = 0x1452759F12C3LL; // Hexadecimal long long literal
+#define DEBUG
+// using namespace std
 
-int main() {
-    int height = 1.8;
+long double largeDecimal = 0x1452579F12C58781; // 0b1100110
+int main()
+{
+    float height = 1.8;
     char character = 'H';
     bool opened = false;
-
     unsigned int count = 0;
-    short score = 0;
+    short score = 30;
     int x, y;
-
     int aux;
-
-    std::cout << "colocando texto entre comillas dobles" << "\n";
+#ifndef DEBUG
+    std::cout << "Escriba dos numeros\n";
     std::cin >> x >> y;
     aux = x;
     x = y;
     y = aux;
-    std::cout << "x: " << x << " y: " << y << std::endl;
-    std::cout << "floating point number: " << height << std::endl;
-    std::cout << "char variable: " << character << std::endl;
-    std::cout << "boolean variable: " << opened << std::endl;
-    std::cout << "long double variable: " << largeDecimal << std::endl;
+    std::cout << "X: " << x << "Y: " << y << std::endl;
+#endif
+#ifdef DEBUG
+    std::cout << "Global variable " << largeDecimal << std::endl;
+    std::cout << "Float variable " << height << std::endl;
+    std::cout << "Char variable " << character << std::endl;
+    std::cout << "bool variable " << opened << std::endl;
+    std::cout << "uns int variable " << count << std::endl;
+    std::cout << "short variable " << score << std::endl;
+#endif
+    /*Multi line comment
+    Imprimir direcciones
+    de variables
+    */
+    std::cout << "Global variable address" << &largeDecimal << std::endl;
+    std::cout << "Float variable address" << &height << std::endl;
+    std::cout << "Char variable address" << &character << std::endl;
+    std::cout << "bool variable address" << &opened << std::endl;
+    std::cout << "uns int variable address" << &count << std::endl;
+    std::cout << "short variable address" << &score << std::endl;
+    /*Multi line comment
+    Imprimir tamaño
+    de variables
+    */
+    std::cout << "Global variable address" << sizeof(largeDecimal) << std::endl;
+    std::cout << "Float variable address" << sizeof(height) << std::endl;
+    std::cout << "Char variable address" << sizeof(character) << std::endl;
+    std::cout << "bool variable address" << sizeof(opened) << std::endl;
+    std::cout << "uns int variable address" << sizeof(count) << std::endl;
+    std::cout << "short variable address" << sizeof(score) << std::endl;
     return 0;
 }
