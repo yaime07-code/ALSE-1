a2150d72619a3d1f268b1d22c50ce9634c17bb0d
yaime07 <jaime.enrique.galvis@gmail.com>
2025-09-13 07:46:43 -0500

    ciclos aaaa

--- ejem4/ciclos.cpp
+++ ejem4/ciclos.cpp
@@ -2,35 +2,49 @@
 using namespace std;
 
 int main() {
-    char userInput = 'q';
+    char userInput;
     int count = 0;
-    cout<<"Ingrese 'q' para salir del ciclo:"<<endl;
-    while (userInput=='y' || userInput=='Y' )
-    {
-        cout<<"deseas continuar (y/n):"<<endl;
-        cin>>userInput;
-        cout <<"Contador vale: "<<count<<endl;
-        count++;    
-    }
-    userInput='q';
-    do
-    {
-        cout<<"deseas continuar oprima q:"<<endl;
-        cin>>userInput;
-        cout <<"Contador vale: "<<count<<endl;
-        count++;
-    } while (userInput=='q' || userInput=='Q');
 
-    short httpResponseCode=200;
-    switch (httpResponseCode)
-    {
-    case 200:
-        cout<<"OK"<<endl;
-        break;
-    case 404:
-        cout<<"Not Found"<<endl;
-        break;
-    case 500:                       
+    cout << "CICLO WHILE" << endl;
+    cout << "Ingrese 'y' para continuar o 'n' para salir:" << endl;
+    cin >> userInput;
+
+    // while: solo entra si el usuario ingresa 'y' o 'Y'
+    while (userInput == 'y' || userInput == 'Y') {
+        cout << "Contador vale: " << count << endl;
+        count++;
+        cout << "¿Deseas continuar (y/n)?: ";
+        cin >> userInput;
+    }
+
+    cout << "\nCICLO DO-WHILE" << endl;
+    count = 0; // reinicio el contador para este ciclo
+
+    // do-while: se ejecuta al menos una vez
+    do {
+        cout << "Contador vale: " << count << endl;
+        count++;
+        cout << "Para continuar oprima 'q', para salir otra tecla: ";
+        cin >> userInput;
+    } while (userInput == 'q' || userInput == 'Q');
+
+    cout << "\nSWITCH CON CODIGOS HTTP" << endl;
+
+    short httpResponseCode = 200;
+    switch (httpResponseCode) {
+        case 200:
+            cout << "OK" << endl;
+            break;
+        case 404:
+            cout << "Not Found" << endl;
+            break;
+        case 500:
+            cout << "Internal Server Error" << endl;
+            break;
+        default:
+            cout << "Unknown Error" << endl;
+            break;
+    }
+
     return 0;
-}   
-    
+}
