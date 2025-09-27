e2ee0b00c97ec8f74b583c3b0277d0d1adb1513f
yaime07 <jaime.enrique.galvis@gmail.com>
2025-09-27 08:06:59 -0500

    fazor

--- cl27_09/imag.cpp
+++ cl27_09/imag.cpp
@@ -1,34 +1,88 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main(){
-    struct Complejo{
-        float real;
-        float imag;
-    };
+struct Complejo {
+float real;
+float imag;
+};
 
-    Complejo num1, num2, suma, resta, producto;
-    cout<<"Ingrese la parte real del primer numero complejo: ";
-    cin>>num1.real;
-    cout<<"Ingrese la parte imaginaria del primer numero complejo: ";
-    cin>>num1.imag;
-    cout<<"Ingrese la parte real del segundo numero complejo: ";
-    cin>>num2.real;
-    cout<<"Ingrese la parte imaginaria del segundo numero complejo: ";
-    cin>>num2.imag;
+// Funciones auxiliares
+float magnitud(Complejo c) {
+return sqrt(c.real * c.real + c.imag * c.imag);
+}
 
-    suma.real = num1.real + num2.real;
-    suma.imag = num1.imag + num2.imag;
+float angulo(Complejo c) {
+return atan2(c.imag, c.real) * 180.0 / M_PI;  // en grados
+}
 
-    resta.real = num1.real - num2.real;
-    resta.imag = num1.imag - num2.imag;
+// División de complejos: (a+bi) / (c+di)
+Complejo division(Complejo a, Complejo b) {
+Complejo res;
+float denom = b.real * b.real + b.imag * b.imag;
+res.real = (a.real * b.real + a.imag * b.imag) / denom;
+res.imag = (a.imag * b.real - a.real * b.imag) / denom;
+return res;
+}
 
-    producto.real = (num1.real * num2.real) - (num1.imag * num2.imag);
-    producto.imag = (num1.real * num2.imag) + (num1.imag * num2.real);
+// Producto de complejos: (a+bi) * (c+di)
+Complejo multiplicacion(Complejo a, Complejo b) {
+Complejo res;
+res.real = (a.real * b.real) - (a.imag * b.imag);
+res.imag = (a.real * b.imag) + (a.imag * b.real);
+return res;
+}
 
-    cout<<"Suma: "<<suma.real<<" + "<<suma.imag<<"i"<<std::endl;
-    cout<<"Resta: "<<resta.real<<" + "<<resta.imag<<"i"<<std::endl;
-    cout<<"Producto: "<<producto.real<<" + "<<producto.imag<<"i"<<std::endl;
+// Suma de complejos
+Complejo sumaC(Complejo a, Complejo b) {
+return {a.real + b.real, a.imag + b.imag};
+}
 
-    return 0;
+// Paralelo: (Z1 * Z2) / (Z1 + Z2)
+Complejo paralelo(Complejo z1, Complejo z2) {
+return division(multiplicacion(z1, z2), sumaC(z1, z2));
+}
+
+int main() {
+Complejo num1, num2, suma, resta, producto, z_paralelo;
+
+```
+cout << "Ingrese la parte real del primer numero complejo: ";
+cin >> num1.real;
+cout << "Ingrese la parte imaginaria del primer numero complejo: ";
+cin >> num1.imag;
+cout << "Ingrese la parte real del segundo numero complejo: ";
+cin >> num2.real;
+cout << "Ingrese la parte imaginaria del segundo numero complejo: ";
+cin >> num2.imag;
+
+// Operaciones
+suma = sumaC(num1, num2);
+
+resta.real = num1.real - num2.real;
+resta.imag = num1.imag - num2.imag;
+
+producto = multiplicacion(num1, num2);
+
+z_paralelo = paralelo(num1, num2);
+
+// Resultados en forma rectangular
+cout << "\n--- Forma rectangular ---\n";
+cout << "Suma: " << suma.real << " + " << suma.imag << "i" << endl;
+cout << "Resta: " << resta.real << " + " << resta.imag << "i" << endl;
+cout << "Producto: " << producto.real << " + " << producto.imag << "i" << endl;
+cout << "Paralelo: " << z_paralelo.real << " + " << z_paralelo.imag << "i" << endl;
+
+// Resultados en forma fasorial (polar)
+cout << "\n--- Forma fasorial (r ∠ θ°) ---\n";
+cout << "Num1: " << magnitud(num1) << " ∠ " << angulo(num1) << "°" << endl;
+cout << "Num2: " << magnitud(num2) << " ∠ " << angulo(num2) << "°" << endl;
+cout << "Suma: " << magnitud(suma) << " ∠ " << angulo(suma) << "°" << endl;
+cout << "Resta: " << magnitud(resta) << " ∠ " << angulo(resta) << "°" << endl;
+cout << "Producto: " << magnitud(producto) << " ∠ " << angulo(producto) << "°" << endl;
+cout << "Paralelo: " << magnitud(z_paralelo) << " ∠ " << angulo(z_paralelo) << "°" << endl;
+
+return 0;
+```
+
 }
