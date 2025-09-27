33ceadb8a0052ef1496c1b8c2932dbf0666317fb
yaime07 <jaime.enrique.galvis@gmail.com>
2025-09-27 07:22:18 -0500

    cl_27_09

---
+++ example22-09/correccionp3.cpp
@@ -0,0 +1,13 @@
+
+
+struct Grade{
+
+    /* data */
+};
+
+float suma 
+
+float calculaPromedio(Grade grades[], int size);= 0;
+    for (int i = 0; i < size; i++) {
+        suma += grades[i];
+    }
